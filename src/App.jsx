a2a3dc23fd661bfccd7843f48928da0b355442b6
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2023-06-26 01:46:41 +0200

    Calendar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
-import { Routes, Route } from "react-router-dom"
-import {useEffect} from "react";
-import { useDispatch } from 'react-redux'
-import {setError, setFlights, setLoading} from "./features/flightReducer";
-import Home from "./routes/Home"
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setError, setFlights, setLoading } from "./features/flightReducer";
+import Home from "./routes/Home";
+import Ticket from "./routes/OrderTicket";
+import Receipt from "./routes/Receipt";
+
 function App() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("flightsData.json");
-                const data = await response.json();
-                dispatch(setFlights(data));
-                dispatch(setLoading(true));
-            } catch (error) {
-                console.error("Problem with fetching data: ", error);
-                dispatch(setError(true));
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("flightsData.json");
+        const data = await response.json();
+        dispatch(setFlights(data));
+        dispatch(setLoading(true));
+      } catch (error) {
+        console.error("Problem with fetching data: ", error);
+        dispatch(setError(true));
+      }
+    };
 
-        fetchData();
+    fetchData();
+  }, []);
 
-    }, []);
-
-
-return (
+  return (
     <>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/orderTicket" element={<Ticket />} />
+        <Route path="/receipt" element={<Receipt />} />
+      </Routes>
     </>
-
-)
+  );
 }
 
-export default App
+export default App;
