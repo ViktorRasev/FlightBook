c823d911472eea75fea02a50c8449c91d228ae34
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2023-06-26 01:59:25 +0200

    ID of the selected flight functionality added, next button disabled if flight has no available seats

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,17 +1,51 @@
 import SearchForm from "../components/SearchForm";
-import Calendar from "../components/Calendar"
-import Button from '@mui/material/Button';
-
-
+import DateAndTimePicker from "../components/DateAndTimePicker.jsx";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 const Home = () => {
-    return(
-        <>
-            <SearchForm />
-            <Calendar />
-            <Button variant="contained">Order Ticket</Button>
-        </>
-    )
-}
+  const navigate = useNavigate();
+  const [resultTicketID, setResultTicketID] = useState(null);
+  const [allSeatsUnavailable, setAllSeatsUnavailable] = useState(false);
+  const allFlights = useSelector((posts) => posts.flights.flights);
 
+  const getTicketID = (ticketID) => {
+    setResultTicketID(ticketID);
+  };
 
-export default Home
+  const navigateToOrderTicket = () => {
+    navigate(`/orderTicket?flightID=${resultTicketID}`);
+  };
+
+  useEffect(() => {
+    if (resultTicketID) {
+      const flightByID = allFlights.find(
+        (flight) => flight.id === parseInt(resultTicketID)
+      );
+      const unavailableSeats = flightByID.seats.every(
+        (seat) => !seat.available
+      );
+      setAllSeatsUnavailable(unavailableSeats);
+    }
+  }, [resultTicketID]);
+
+  return (
+    <>
+      <SearchForm />
+      <DateAndTimePicker getTicketID={getTicketID} />
+      <div style={{ width: "100%" }}>
+        <Button
+          sx={{ display: "block", width: "200px", margin: "0 auto" }}
+          variant="contained"
+          disabled={!resultTicketID || allSeatsUnavailable}
+          onClick={navigateToOrderTicket}
+        >
+          Order Ticket
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
