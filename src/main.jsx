5aba08c736c31584f472d3484421e9929d2321fc
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2023-06-26 02:01:29 +0200

    rtk persist added to prevent loosing state on page refresh

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import './index.css'
 import { Provider } from 'react-redux';
 import store  from "./app/store.js"
 import { BrowserRouter } from "react-router-dom";
+import {PersistGate} from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
+
+
+let persistor = persistStore(store)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
       <BrowserRouter>
           <Provider store={store}>
+              <PersistGate persistor={persistor}>
                 <App />
+              </PersistGate>
           </Provider>
       </BrowserRouter>
-  </React.StrictMode>,
 )
