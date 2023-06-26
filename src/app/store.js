51a672623f25abd0f4221bd18732dd765f2a4b5c
ViktorRasev <103742526+ViktorRasev@users.noreply.github.com>
2023-06-26 02:10:51 +0200

    rtk persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware  } from "@reduxjs/toolkit"
 import flightsReducer from "../features/flightReducer"
 import pickedDestinationsReducer from "../features/pickedDestinationsReducer";
+import resultTicketReducer from "../features/resultTicketReducer";
+import storage from "redux-persist/lib/storage"
+import {persistReducer} from "redux-persist"
+import {combineReducers} from "@reduxjs/toolkit"
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage
+}
+
+
+const reducer = combineReducers({
+    flights: flightsReducer,
+    pickedDestinations: pickedDestinationsReducer,
+    resultTicket: resultTicketReducer
+
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 const store = configureStore({
-    reducer: {
-        flights: flightsReducer,
-        pickedDestinations: pickedDestinationsReducer
-    }
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: false,
+    }),
 })
 
 export default store
